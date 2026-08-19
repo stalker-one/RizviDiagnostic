27e699bdaf44b6d47b775caa74e1378646ca7b01
stalker-one <stalkermianone10@gmail.com>
2026-08-20 01:58:51 +0500

    Fix native Android plugin registration before Capacitor bridge initialization

--- frontend/android/app/src/main/java/com/rizvi/diagnosticcenter/MainActivity.java
+++ frontend/android/app/src/main/java/com/rizvi/diagnosticcenter/MainActivity.java
@@ -1,13 +1,14 @@
 package com.rizvi.diagnosticcenter;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    public void onCreate(android.os.Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
         registerPlugin(PrintPlugin.class);
         registerPlugin(UpdatePlugin.class);
         registerPlugin(ExportPlugin.class);
+        super.onCreate(savedInstanceState);
     }
 }
